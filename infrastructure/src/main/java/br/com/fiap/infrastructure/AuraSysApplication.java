ae0e613740a7ec19f465df3b3a57945cf0174d38
luizfilipebp <luizfilipe098@gmail.com>
2025-07-14 21:08:21 -0300

    commit inicial

---
+++ infrastructure/src/main/java/br/com/fiap/infrastructure/AuraSysApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraSysApplication.class, args);
+    }
+
+}
